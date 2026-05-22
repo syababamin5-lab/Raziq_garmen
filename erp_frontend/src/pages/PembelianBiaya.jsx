4c529db04515bbf2df0c5ff623c7b7ac97a87163
Syabaab Amin <syababamin5@gmail.com>
2026-05-22 14:54:51 +0700

    feat: tambahkan akun Kewajiban (21210 Hutang Gaji) ke dropdown Pengeluaran Biaya untuk pelunasan upah cutting

--- erp_frontend/src/pages/PembelianBiaya.jsx
+++ erp_frontend/src/pages/PembelianBiaya.jsx
@@ -72,7 +72,14 @@ export default function PembelianBiaya() {
       ]);
       setSuppliers(resSupp.filter(m => m.kategori === 'Supplier Bahan Baku'));
       setInventory(resInv.filter(b => b.kategori !== 'Barang Jadi (Baju)'));
-      setCoa(resCoa.filter(a => a.kategori === 'Beban'));
+      // Tampilkan akun Beban, HPP/Biaya Produksi, dan Kewajiban (untuk bayar utang/gaji)
+      setCoa(resCoa.filter(a => 
+        a.kode_akun.startsWith('5') || 
+        a.kode_akun.startsWith('6') || 
+        a.kode_akun.startsWith('2') || 
+        a.kategori === 'Beban' || 
+        a.kategori === 'Kewajiban'
+      ));
       if (resAset.success) {
         setAsetSummary(resAset.data.summary);
         setSusutStatus(resAset.data.status_susut || { sudah_susut: false, jumlah_susut: 0, bulan: '' });
